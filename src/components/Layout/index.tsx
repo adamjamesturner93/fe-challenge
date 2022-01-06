77a9a64e84e47bf7d844a21169b63ccdedbf150e
Adam Turner <adam.turner@qualio.com>
2022-01-06 17:45:29 +0000

    Added semantic markup to layout and initial styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,30 +1,33 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const Layout = ({ children }: Props) => (
-  <div className="container mx-auto px-4">
-    <div className="flex justify-between">
-      <strong>Qogita</strong>
-      <nav>
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">
-              <a className="underline">Products</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/cart">
-              <a className="underline">Your Cart</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="w-full flex flex-col h-screen bg-gray-200">
+        <header className="px-8 flex justify-between h-16 items-center bg-gray-700 text-gray-50">
+          <strong>Qogita</strong>
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <Link href="/">
+                  <a className="underline">Products</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/cart">
+                  <a className="underline">Your Cart</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="px-8">
+          {children}
+        </main>
     </div>
-    {children}
-  </div>
-);
 
-export default Layout;
+)
+
+export default Layout
